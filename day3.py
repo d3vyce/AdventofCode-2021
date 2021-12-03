e0633a5ac175c77ce671d576925a01cb5765cf72
Nicolas Sudres <nicolas.sudres@hotmail.fr>
2021-12-03 12:24:08 +0100

    fix part 2 + clear code

--- day3.py
+++ day3.py
@@ -1,5 +1,14 @@
 # Advent of Code 2021, Day 3, d3vyce
 
+def test(search, pos, mode):
+    count = 0
+    for i in search:
+        if(int(i[pos]) == 1): count += 1
+
+    if(count >= len(search)/2 and mode == "more" or count <= len(search)/2 and mode == "less"): return 1
+    else: return 0
+
+
 with open('input/day3.txt') as f:
     file = [i for i in f.read().splitlines()]
 
@@ -23,23 +32,18 @@ for i in count:
 print("Part 1 : ", int(gamma, 2)*int(epsilon, 2))
 
 # Part 2
-print(gamma)
-
 search_g = file
 
 for i in range(len(gamma)):
-    search_g = [y for y in search_g if int(y[i]) == int(gamma[i])]
+    byte = test(search_g, i, "more")
+    search_g = [y for y in search_g if int(y[i]) == byte]
     if(len(search_g) == 1): break
 
-print(search_g)
-
 search_e = file
-print(epsilon)
 
 for i in range(len(epsilon)):
-    search_e = [y for y in search_e if int(y[i]) == int(epsilon[i])]
-    if(len(search_e) == 6): break
+    byte = test(search_e, i, "less")
+    search_e = [y for y in search_e if int(y[i]) == byte]
+    if(len(search_e) == 1): break
 
-print(search_e)
-
-print("Part 1 : ", int(search_g[0], 2))
+print("Part 2 : ", int(search_g[0], 2)*int(search_e[0], 2))
