5f94eaca6242c7fdbe5ca01aa74a8b0e37da78c2
Nicolas Sudres <nicolas.sudres@hotmail.fr>
2021-12-03 13:05:07 +0100

    code optimisation

--- day3.py
+++ day3.py
@@ -1,12 +1,12 @@
 # Advent of Code 2021, Day 3, d3vyce
 
-def test(search, pos, mode):
+def search_b(search, pos, mode):
     count = 0
     for i in search:
         if(int(i[pos]) == 1): count += 1
 
-    if(count >= len(search)/2 and mode == "more" or count <= len(search)/2 and mode == "less"): return 1
-    else: return 0
+    if(count >= len(search)/2 and mode == "more" or count < len(search)/2 and mode == "less"): return '1'
+    else: return '0'
 
 
 with open('input/day3.txt') as f:
@@ -17,17 +17,9 @@ count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 gamma = ""
 epsilon = ""
 
-for i in file:
-    for y in range(len(i)):
-        if(int(i[y]) == 1):count[y] += 1
-
-for i in count:
-    if(i < 500):
-        gamma += '0'
-        epsilon += '1'
-    else:
-        gamma += '1'
-        epsilon += '0'
+for i in range(len(file[0])):
+    gamma += search_b(file, int(i), "more")
+    epsilon += search_b(file, int(i), "less")
 
 print("Part 1 : ", int(gamma, 2)*int(epsilon, 2))
 
@@ -35,14 +27,14 @@ print("Part 1 : ", int(gamma, 2)*int(epsilon, 2))
 search_g = file
 
 for i in range(len(gamma)):
-    byte = test(search_g, i, "more")
+    byte = int(search_b(search_g, i, "more"))
     search_g = [y for y in search_g if int(y[i]) == byte]
     if(len(search_g) == 1): break
 
 search_e = file
 
 for i in range(len(epsilon)):
-    byte = test(search_e, i, "less")
+    byte = int(search_b(search_e, i, "less"))
     search_e = [y for y in search_e if int(y[i]) == byte]
     if(len(search_e) == 1): break
 
