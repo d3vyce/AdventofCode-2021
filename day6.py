5c2efe802a584de557106025dc4822e137f91c33
Nicolas S <44915747+d3vyce@users.noreply.github.com>
2021-12-06 11:01:00 +0100

    optimisation for day 6

--- day6.py
+++ day6.py
@@ -2,6 +2,19 @@
 
 from collections import defaultdict
 
+def count(dic, days):
+    for day in range(days):
+        dic_tmp = defaultdict(int)
+        for i, j in dic.items():
+            i -= 1
+            if i < 0:
+                dic_tmp[6] += j
+                dic_tmp[8] += j
+            else:
+                dic_tmp[i] += j
+        dic = dic_tmp
+    return sum(dic.values())
+
 with open('input/day6.txt') as f:
     file = [int(i) for i in f.read().split(',')]
 
@@ -9,20 +22,8 @@ dic = defaultdict(int)
 for i in file:
     dic[i] += 1
 
-print(sum(dic.values()))
+# Part 1
+print("Part 1 : ", count(dic, 80))
 
-days = 256
-
-# Part 1&2
-for day in range(days):
-    dic_tmp = defaultdict(int)
-    for i, j in dic.items():
-        i -= 1
-        if i < 0:
-            dic_tmp[6] += j
-            dic_tmp[8] += j
-        else:
-            dic_tmp[i] += j
-    dic = dic_tmp
-
-print(sum(dic.values()))
+# Part 2
+print("Part 2 : ", count(dic, 256))
