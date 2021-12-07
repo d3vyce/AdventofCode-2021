9314f8d43783cda720e505765735c841f19b2c95
Nicolas Sudres <nicolas.sudres@hotmail.fr>
2021-12-08 00:16:00 +0100

    code optimisation

--- day7.py
+++ day7.py
@@ -1,20 +1,18 @@
 # Advent of Code 2021, Day 7, d3vyce
 
+import numpy as np
+
 with open('input/day7.txt') as f:
     file = [int(i) for i in f.read().split(',')]
 
 # Part 1
 fuel = 0
-tmp = 0
 
-for i in range(min(file), max(file)):
-    for y in file:
-        if(y < i): tmp += i - y
-        else: tmp += y - i
-    if(fuel > tmp or fuel == 0): fuel = tmp
-    tmp = 0
+mediane = np.median(file)
+for y in file:
+    fuel += np.abs(y - mediane)
     
-print("Part 1 : ", fuel)
+print("Part 1 : ", int(fuel))
 
 # Part 2
 fuel = 0
@@ -22,9 +20,8 @@ tmp = 0
 
 for i in range(min(file), max(file)):
     for y in file:
-        if(y < i): tmp += sum(range((i - y)+1))
-        else: tmp += sum(range((y - i)+1))
+        tmp += abs(y - i)*(1+abs(y - i))/2
     if(fuel > tmp or fuel == 0): fuel = tmp
     tmp = 0
     
-print("Part 2 : ", fuel)
+print("Part 2 : ", int(fuel))
