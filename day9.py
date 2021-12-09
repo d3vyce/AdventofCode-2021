1553c66946dd92a209dcfdfce19e7f4fe668d75b
Nicolas Sudres <nicolas.sudres@hotmail.fr>
2021-12-09 14:07:48 +0100

    part 1 optimisation

--- day9.py
+++ day9.py
@@ -1,30 +1,35 @@
 # Advent of Code 2021, Day 9, d3vyce
 
+from collections import deque
+
+def check_coord(x, y, matrix):
+    x_max = len(file)
+    y_max = len(file[0])
+
+    if(x < 0 or x >= x_max or y < 0 or y >= y_max): return 10
+    else : return int(matrix[x][y])
+
 with open('input/day9.txt') as f:
     file = [i for i in f.read().splitlines()]
 
 x_max = len(file)
 y_max = len(file[0])
+
+# Part 1
 result = 0
-tmp = 0
-check = 4
 
 for x in range(x_max):
     for y in range(y_max):
-        if(x == 0): check -= 1
-        if(x == 99): check -= 1
-        if(y == 0): check -= 1
-        if(y == 99): check -= 1
-
-        if(x != 0 and file[x][y] < file[x-1][y]): tmp += 1
-        if(x != 99 and file[x][y] < file[x+1][y]): tmp += 1
-        if(y != 0 and file[x][y] < file[x][y-1]): tmp += 1
-        if(y != 99 and file[x][y] < file[x][y+1]): tmp += 1
-        if(tmp == check): 
-            result += int(file[x][y])+1
-        tmp = 0
-        check = 4
+        val = check_coord(x, y, file)
+        if(
+            val < check_coord(x-1, y, file)
+            and val < check_coord(x+1, y, file)
+            and val < check_coord(x, y-1, file)
+            and val < check_coord(x, y+1, file)
+        ): result += int(file[x][y])+1
+      
 
 print("Part 1 : ", result)
 
-#if(file[x][y] < file[x+1][y] and file[x][y] < file[x-1][y] and file[x][y] < file[x][y+1] and file[x][y] < file[x][y-1]): result += 1
+# Part 2
+print("Part 2 : ", result)
