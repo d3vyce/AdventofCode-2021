1f14e0b57a5dfa3da67d78b07b09373774951535
Nicolas Sudres <nicolas.sudres@hotmail.fr>
2021-12-09 15:08:57 +0100

    add day 9 part 2

--- day9.py
+++ day9.py
@@ -9,6 +9,40 @@ def check_coord(x, y, matrix):
     if(x < 0 or x >= x_max or y < 0 or y >= y_max): return 10
     else : return int(matrix[x][y])
 
+def limit(x, y, matrix):
+    result = check_coord(x, y, matrix)
+    if(result < 9): return result
+    else: return -1
+
+def check_basins(x, y, matrix):
+    x_max = len(file)
+    y_max = len(file[0])
+
+    basins_loc = set()
+    loc_to_check = []
+    loc_to_check.append((x, y))
+
+    while len(loc_to_check) > 0:
+        x, y = loc_to_check.pop()
+
+        if (x, y) in basins_loc:
+            continue
+
+        basins_loc.add((x, y))
+
+        val = limit(x, y, matrix)
+        if limit(x - 1, y, matrix) > val:
+            loc_to_check.append((x - 1, y))
+        if limit(x + 1, y, matrix) > val:
+            loc_to_check.append((x + 1, y))
+        if limit(x, y - 1, matrix) > val:
+            loc_to_check.append((x, y - 1))
+        if limit(x, y + 1, matrix) > val:
+            loc_to_check.append((x, y + 1))
+
+    return len(basins_loc)
+
+
 with open('input/day9.txt') as f:
     file = [i for i in f.read().splitlines()]
 
@@ -17,6 +51,7 @@ y_max = len(file[0])
 
 # Part 1
 result = 0
+low_pt = []
 
 for x in range(x_max):
     for y in range(y_max):
@@ -26,10 +61,20 @@ for x in range(x_max):
             and val < check_coord(x+1, y, file)
             and val < check_coord(x, y-1, file)
             and val < check_coord(x, y+1, file)
-        ): result += int(file[x][y])+1
+        ): 
+            result += int(file[x][y])+1
+            low_pt.append((x, y))
       
 
 print("Part 1 : ", result)
 
 # Part 2
+basins = []
+
+for x, y in low_pt:
+    basins.append(check_basins(x, y, file))
+
+basins.sort(reverse=True)
+result = basins[0] * basins[1] * basins[2]
+
 print("Part 2 : ", result)
